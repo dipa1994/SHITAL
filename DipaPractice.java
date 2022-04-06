743864003b4498fc7a80bb8e5a69c73ed953acee
DIPA <dipa.smvs@gmail.com>
2022-04-06 19:55:33 +0100

    HELLO

---
+++ DipaPractice.java
@@ -0,0 +1,12 @@
+package com.dipa.learning;
+
+public class DipaPractice {
+    public static void main(String[] args) {
+
+        System.out.println("I am Dipa Prajapati");
+        System.out.println("Hello Summer");
+        System.out.println("Welcome to Java Programme");
+
+    }
+
+}
