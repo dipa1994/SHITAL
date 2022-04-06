e9895b13d112228cf375aa5719c7cbcdd1c1af2a
Dipa <dipa.smvs@gmail.com>
2022-04-06 22:07:30 +0100

    third change

--- PracticeDips.java
+++ PracticeDips.java
@@ -3,7 +3,7 @@ package com.dipa.learning;
 public class PracticeDips {
     public static void main(String[] args) {
 
-        System.out.println("I am Leaning Java");
+        System.out.println("I am Learning Java");
         System.out.println("Hello Summer");
         System.out.println("Thank You");
 
