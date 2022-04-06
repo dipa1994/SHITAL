969428004d01e1b3c99929c72ec15f3c32854fd2
DIPA <dipa.smvs@gmail.com>
2022-04-06 21:19:58 +0100

    Second change

---
+++ PracticeDips.java
@@ -0,0 +1,11 @@
+package com.dipa.learning;
+
+public class PracticeDips {
+    public static void main(String[] args) {
+
+        System.out.println("I am Leaning Java");
+        System.out.println("Hello Summer");
+        System.out.println("Thank You");
+
+    }
+}
